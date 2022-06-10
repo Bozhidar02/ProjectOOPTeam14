bb3f96fe081810ef4f36fc15c51ab37898621d86
Svetlozar S <stormcrafterbg@gmail.com>
2022-06-10 12:44:52 +0300

    Added player header and a dummy inventory

---
+++ ProjectOOPTeam14/Player.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "Inventory.h"
+#include "String.h"
+
+const int ARMOR_SLOTS = 3;
+const float LVL_ONE_XP = 100;
+
+class Player
+{
+private:
+	MyString::String username;
+	MyString::String password;
+
+	float xp;
+	float max_xp;
+	unsigned int level;
+	float health;
+
+	Weapon* weapon;
+	Armor** armor;
+	
+	Inventory inventory;
+
+	void copy();
+	void free();
+
+protected:
+	enum Type
+	{
+		WARRIOR,
+		ARCHER,
+		MAGE,
+		UNKNOWN
+	} type;
+public:
+	Player(MyString::String& username, MyString::String& password);
+	Player(const Player& other);
+	Player& operator=(const Player& other);
+	~Player();
+
+	const MyString::String& GetName() const;
+	bool IsUser(const MyString::String& name, const MyString::String& pass) const;
+	unsigned int GetLevel() const;
+	float GetHealth() const;
+
+	void AddXP(const float xp);
+
+	bool PickWeapon(const MyString::String& weaponName);
+	bool SetArmorSlot(const MyString::String& armorName, const int slot);
+
+	virtual bool Attack(const Player& other) = 0;
+	virtual bool TakeDamage(const Player& other) = 0;
+};
+
